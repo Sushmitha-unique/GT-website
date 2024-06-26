aa878afd8bab6a86daab278dbdf7e01d4f46dba8
Induja0604 <indujanarnavaram@gmail.com>
2024-06-26 14:39:38 +0530

    stage1

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="https://media.licdn.com/dms/image/C4D16AQFHXSba8Nf89g/profile-displaybackgroundimage-shrink_200_800/0/1655103837481?e=2147483647&v=beta&t=6Jo4c8zN93e6xPmmkdIA6o8spzNTBoRCWjOWYe_tqi8" alt="Logo" />
+      </div>
+      <div className="navbar-links">
+        <a href="#home">Home</a>
+        <div className="dropdown">
+          <a href="#courses" className="dropbtn">Courses</a>
+          <div className="dropdown-content">
+            <a href="#web-development">Web Development</a>
+            <a href="#python">Python</a>
+            <a href="#Java">Java</a>
+          </div>
+        </div>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#login" className="login">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
